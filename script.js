5db6a6a0359fbbac90f02ee446c2150108771d93
Jared Peterson <jaredpeterson50@gmail.com>
2022-08-29 08:43:14 -0700

    updated everything on hunt

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//toDo ... add logic for computer to bid 2 with a two and 3 with an ace.
 class Card{
     constructor(suit, value){
         this.value = value;
@@ -6,24 +5,56 @@ class Card{
     }
 }
 
+class Player{
+    constructor(){
+        this.bid=0;
+        this.bidSuit="";
+        this.offense=true; //false if east or west wins bid
+    }
+}
+
+class Gameplay{
+    constructor(){
+        highBid = 0;
+        eastWest = 0;
+        northSouth = 0;
+    }
+}
+
+var p0 = new Player();
+var p1 = new Player();
+var p2 = new Player();
+var p3 = new Player();
+
 //card deck
 let suits = ["clubs","spades","diamonds","hearts"];
 let values = [2,3,4,5,6,7,8,9,10,11,12,13,14];
 let deck = [];
-let players = [[],[],[],[]]; //all 4 player in the players array
+let players = [[],[],[],[]]; //holds the players cards west,south,east,north
+
 
 //gameplay variables
 //dealer order is 0=west 1=south 2=east 3=north
+document.getElementById("playerSuitSelection").classList.add("hidden"); //make suit selection hidden default
+document.getElementById("discard").classList.add("hidden");
+document.getElementById("computerWonBid").classList.add("hidden");
 var dealerOrder = ['west', 'south', 'east', 'north']
 var currentDealerIndex = 0; //west intitial
 var currentBidderIndex = 1; //south initial
 var currentHighBid = 0;
+var winningSuit ="";
+var winningBidder = "";
+var timer = ms => new Promise(res => setTimeout(res, ms))
+var delay = 1000;
+var playerBidAmounts = [0,0,0,0];
+var bidCounter=0;
+var discards = [];
 
 function startGame(){
     createDeck();
     shuffle();
     deal();
-    sortBySuite();
+    sortBySuit();
     showCards();
     bid();
 }
@@ -62,12 +93,12 @@ function deal(){
     }
 }
 
-function sortBySuite(){
-    let suits = ["clubs","spades","diamonds","hearts", "joker"]; //added a joker suite
+function sortBySuit(){
+    let suits = ["clubs","spades","diamonds","hearts", "joker"]; //added a joker suit
     //loop through each player
     for(let k = 0; k < 4; k++){
         let sortedArray = [];
-        //loop for each suite
+        //loop for each suit
         for(let j = 0; j < suits.length; j++){
             //loop through each card in their hand
             for(let i = 0; i < 6; i++){
@@ -95,35 +126,49 @@ function showCards(){
         }
     }
 }
-function bidBtn(bidAmt){
+
+function bidBtnHelper(bidAmount){
+    document.getElementById("southValue").innerText=bidAmount;
+    document.getElementById("bidValue").innerText=bidAmount;
+    currentHighBid=Number.parseInt(bidAmount);
+    winningBidder=dealerOrder[currentBidderIndex];
+}
+function userBid(bidAmt){
+    playerBidAmounts[1] = bidAmt;
+    console.log("userbidAmount: " + bidAmt);
     switch(bidAmt){
         case '2':
-            document.getElementById("southValue").innerText='2';
-            document.getElementById("bidValue").innerText='2';
+            if(currentHighBid < 2){
+                bidBtnHelper(bidAmt);
+            }
             currentBidderIndex++;
             bid();
             break;
         case '3':
-            document.getElementById("southValue").innerText='3';
-            document.getElementById("bidValue").innerText='3';
+            if(currentHighBid < 3){
+                bidBtnHelper(bidAmt);
+            }
             currentBidderIndex++;
             bid();
             break;
         case '4':
-            document.getElementById("southValue").innerText='4';
-            document.getElementById("bidValue").innerText='4';
+            if(currentHighBid < 4){
+                bidBtnHelper(bidAmt);
+            }
             currentBidderIndex++;
             bid();
             break;
         case '5':
-            document.getElementById("southValue").innerText='5';
-            document.getElementById("bidValue").innerText='5';
+            if(currentHighBid < 5){
+                bidBtnHelper(bidAmt);
+            }
             currentBidderIndex++;
             bid();
             break;
         case '6':
-            document.getElementById("southValue").innerText='6';
-            document.getElementById("bidValue").innerText='6';
+            if(currentHighBid < 6){
+                bidBtnHelper(bidAmt);
+            }
             currentBidderIndex++;
             bid();
             break;
@@ -143,64 +188,121 @@ function bidBtn(bidAmt){
 if current bidder and dealer+1 are the same then exit the loop everyone bid alread. if condition is to exit
 the loop if its the human players turn. after the onlick event executes it will return to the bid function
 and loop will continue. the CPU logic will refenernce a logic function for pitch bidding strategy */
-function bid(){
-    document.getElementById("dealerValue").innerText=dealerOrder[currentDealerIndex];
-    document.getElementById("bidderValue").innerText=dealerOrder[currentBidderIndex];    
-    while(currentDealerIndex+1 != currentBidderIndex){ //stopping condition is everyone has bid including dealer
-        if(currentBidderIndex == 1){ //exit the loop if its south turn to click. then re enter function
+
+
+async function bid(){
+    document.getElementById("dealerValue").innerText=dealerOrder[currentDealerIndex]; //display dealer
+    document.getElementById("bidderValue").innerText=dealerOrder[currentBidderIndex]; //display bidder
+
+    while(bidCounter < 4){
+        bidCounter++;
+        if(currentBidderIndex === 1){  //exit the loop if its the users turn to bid. then re enter function
+            await timer(delay);
             return;
         }
         else{
-            const bidAmount = bidLogic();
-            document.getElementById(`${dealerOrder[currentBidderIndex]}Value`).innerText=bidAmount;
+            const bidObj = bidLogic(); //computer decides what to bid
+            if(bidObj.bid > currentHighBid){
+                currentHighBid = bidObj.bid;
+                console.log("updating current high bid: " + currentHighBid);
+                winningSuit=bidObj.suit;
+                winningBidder=dealerOrder[currentBidderIndex];
+            }
+            document.getElementById(`${dealerOrder[currentBidderIndex]}Value`).innerText=bidObj.bid;
+            document.getElementById("bidderValue").innerText=dealerOrder[currentBidderIndex]; 
+            document.getElementById("bidValue").innerText=currentHighBid;
         }
+        await timer(delay);
         currentBidderIndex = (currentBidderIndex + 1) % 4;
     }
-    
-//currentDealerIndex = (currentDealerIndex + 1) % 4; //after each game the dealer moves right
-//currentBidderIndex = (currentDealerIndex + 1) % 4; //set the first bidder after each game
+   chooseSuit();
 }
+
 function bidLogic(){
     let bidAmount = 0;
-    let suiteBidAmount = [0,0,0,0]; //init to zero each value
-    console.log("current bidder index: " + currentBidderIndex);
-    console.log("dealerorder[currentBidderIndex]: " + dealerOrder[currentBidderIndex]);
-    console.log("player cards");
-    console.log(players[currentBidderIndex][0].value);
-    console.log(players[currentBidderIndex][1].value);
-    //console.log("index of hearts: " + suits.indexOf(players[currentBidderIndex][i].suit));
-    //each rule will loop like this let first check for Aces dynamic programming we dont need 3 loops
-    //each suite
+    let suitBidAmount = [0,0,0,0]; //init to zero each value
+    //go thru each card putting values of points into the suitBidAmount array
     for(let i = 0; i < 6; i++){
         if(players[currentBidderIndex][i].value==14) //we have an ace what suite? hears lets say
-            suiteBidAmount[suits.indexOf(players[currentBidderIndex][i].suit)] += 2;
+            suitBidAmount[suits.indexOf(players[currentBidderIndex][i].suit)] += 2;
         if(players[currentBidderIndex][i].value==2) // we have a low
-            suiteBidAmount[suits.indexOf(players[currentBidderIndex][i].suit)] += 1.1;
+            suitBidAmount[suits.indexOf(players[currentBidderIndex][i].suit)] += 1.1;
         if(players[currentBidderIndex][i].value==20){// we have a joker every suite .5 add
             for(let j = 0; j < 4; j++)
-                suiteBidAmount[j] += .5;
+                suitBidAmount[j] += .5;
         }
         if(players[currentBidderIndex][i].value==11){ //jack add .5 to both trump and off suite 0,1 or ,2,3
-            suiteBidAmount[suits.indexOf(players[currentBidderIndex][i].suit)] += .5;   
+            suitBidAmount[suits.indexOf(players[currentBidderIndex][i].suit)] += .5;   
         }
         if(players[currentBidderIndex][i].value==12){ //queen add .25
-            suiteBidAmount[suits.indexOf(players[currentBidderIndex][i].suit)] += .25; 
+            suitBidAmount[suits.indexOf(players[currentBidderIndex][i].suit)] += .25; 
         }
         if(players[currentBidderIndex][i].value==13){ //king add .25
-            suiteBidAmount[suits.indexOf(players[currentBidderIndex][i].suit)] += .25; 
+            suitBidAmount[suits.indexOf(players[currentBidderIndex][i].suit)] += .25; 
         }      
-
     }
-    console.log("suite bid amount: " + suiteBidAmount);
-    //take the ceiling of the largest number in the array
-    bidAmount= Math.max.apply(Math, suiteBidAmount);
-    bidAmount = Math.ceil(bidAmount);
-    console.log("going to bid:" + bidAmount);
-    if(currentHighBid < bidAmount){
-        currentHighBid = bidAmount
-        return bidAmount;
+    console.log("bid counter: " + bidCounter);
+    bidAmount= Math.max.apply(Math, suitBidAmount);
+    let roundedBidAmount = Math.ceil(bidAmount); //take the ceiling of the largest number in the array to bid
+    if(currentHighBid >= roundedBidAmount || roundedBidAmount < 2)//not a new high bidder or dont have a 2 bid
+        roundedBidAmount="pass";
+    if(currentHighBid < 2 && bidCounter == 3) //if your the last bidder you need to bid 2 and dont over bid
+        roundedBidAmount=2;
+    const retVal = {
+        bid: roundedBidAmount,
+        suit: suits[suitBidAmount.indexOf(bidAmount)]
+    }
+    return retVal;
+}
+
+//if winningBidder=== "south") display window or buttons
+function chooseSuit(userSuit){
+    document.getElementById("biddingTable").classList.add("hidden");
+    document.getElementById("discard").classList.remove("hidden");
+    //allow the user to discard
+    let player1Cards = document.querySelectorAll(".player1");
+    for(let i = 0; i < player1Cards.length; i++){
+        player1Cards[i].addEventListener('click', () => {
+            player1Cards[i].classList.add("translateY");
+            discards.push(player1Cards[i].classList[2].toString());
+        });
+    }
+    //check if user won bid or computer
+    if(winningBidder === 'south'){
+        winningSuit = userSuit;
+        document.getElementById("trumpSuit").innerText= `${winningSuit}`;
+        document.getElementById("playerSuitSelection").classList.remove("hidden");
     }
     else{
-        return "pass"
+        let node = document.getElementById("computerWonBid");
+        node.innerText = `${winningBidder} won the bid in  ${winningSuit} with a bid of ${currentHighBid}`;
+        document.getElementById("trumpSuit").innerText= `${winningSuit}`;
+        document.getElementById("computerWonBid").classList.remove("hidden")
+        discardLogic();
+    }    
+}
+function discardLogic(){
+    console.log("computer discard logic");
+    let player0Cards = document.querySelectorAll(".player");
+    for(let i = 0; i < player0Cards.length; i++){
+        console.log(player0Cards.length);
     }
+}
+
+function reDeal(){
+    console.log(discards);
+    console.log(players[discards[0][1]][discards[0][3]]);
+    for(let i = 0; i < discards.length; i++){
+        //locate the card in players array, create a new card deck.pop(). re render the cards
+        //delete the element div then launch showCards()
+        let temp = deck.pop();
+        players[discards[i][1]][discards[i][3]] = temp;// ex p1c2  leter 1 is 1 letter 3 is 2
+    }
+    //every card has class cardPlayer, querry them and remove them all
+    let test = document.querySelectorAll('.cardPlayer');
+    for(let i = 0; i < test.length; i++){
+        test[i].remove();
+    }
+    //re run show cards with new cards removing discards
+    showCards();
 }
